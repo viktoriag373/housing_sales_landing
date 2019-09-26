65a004c5d901ead6c7025ad9fcd3aa76442311d9
Victoria <viktoriag373@gmail.com>
2019-09-26 18:49:43 +0300

    1. Добавлены якоря. 2. Добавлено модальное окно "Оставить заявку".

--- js/script.js
+++ js/script.js
@@ -1,26 +1,73 @@
 "use strict"
 window.onload = function() {
-    var buttonsCall = document.querySelectorAll('.button_call');
-    var modalDialogCallMe = document.querySelector('.modal_dialog_call_me');
-    var modalDialogCallMeCloseButton = document.querySelector('.modal_dialog_call_me_close_button');
+    initCallMeDialog();
+    initSingUpForViewingDialog();   
+}
+
+
+function initCallMeDialog() {
+    let buttonsCall = document.querySelectorAll('.button_call');
+    let modalDialogCallMe = document.querySelector('.modal_dialog_call_me');
+    let modalDialogCallMeCloseButton = document.querySelector('.modal_dialog_call_me_close_button');
 
     buttonsCall.forEach(element => {
-        element.onclick = openModal;
+        element.onclick = openModalCallMe;
     });
 
-    modalDialogCallMeCloseButton.onclick = closeModal;
-
+    modalDialogCallMeCloseButton.onclick = closeModalCallMe;
     
-    function openModal() {
+    function openModalCallMe() {
         modalDialogCallMe.classList.add('modal_dialog_call_me_visible');
-        window.document.body.classList.add('disable_scroll');
+        // window.document.body.classList.add('disable_scroll');
     }
 
-    function closeModal() {
+    function closeModalCallMe() {
         modalDialogCallMe.classList.remove('modal_dialog_call_me_visible');
-        window.document.body.classList.remove('disable_scroll');
+        // window.document.body.classList.remove('disable_scroll');
 
     }
-
-
 }
+
+function initSingUpForViewingDialog() {
+    let modalDialogSignUpForViewing = document.querySelector('.modal_dialog_sign_up_for_viewing');
+    let firstFormButton = document.querySelector('.first_form_button');
+    let modalDialogSignUpForViewingBodyButton = document.querySelector('.modal_dialog_sign_up_for_viewing_body_button');
+    let modalDialogSignUpForViewingCloseButton = document.querySelector('.modal_dialog_sign_up_for_viewing_close_button');
+    let firstFormInputName = document.querySelector('.first_form_input_name');
+    let firstFormInputNumber = document.querySelector('.first_form_input_number');
+
+    firstFormButton.onclick = checkValues;
+
+    modalDialogSignUpForViewingBodyButton.onclick = closeModalSignUpForViewing;
+    modalDialogSignUpForViewingCloseButton.onclick = closeModalSignUpForViewing;
+
+    function checkValues() {
+        let reg = /^[A-ZА-Я]*$/gi;
+        if (firstFormInputName.value == '' || !reg.test(firstFormInputName.value)) {
+            firstFormInputName.style.border = '2px solid red'; 
+            return;  
+        } else {
+            firstFormInputName.style.border = 'none'; 
+        }
+
+        if (firstFormInputNumber.value == '' || isNaN(firstFormInputNumber.value)) {
+            firstFormInputNumber.style.border = '2px solid red'; 
+            return
+        } else {
+            firstFormInputName.style.border = 'none';
+            openModalSignUpForViewing();
+        }
+    }
+
+    function openModalSignUpForViewing() {
+        modalDialogSignUpForViewing.classList.add('modal_dialog_sign_up_for_viewing_visible');
+        // window.document.body.classList.add('disable_scroll');
+    }
+
+    function closeModalSignUpForViewing() {
+        modalDialogSignUpForViewing.classList.remove('modal_dialog_sign_up_for_viewing_visible');
+        // window.document.body.classList.remove('disable_scroll');
+
+    }
+}
+
